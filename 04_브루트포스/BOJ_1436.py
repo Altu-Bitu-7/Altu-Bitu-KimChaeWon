92cc154b33e0fb6e0bef20562696b9031aa70cdb
chaewonkim <kimlove9182@gmail.com>
2024-09-10 03:28:52 +0900

    [04_브루트포스] 9월 10일

---
+++ 04_브루트포스/BOJ_1436.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+num = 0
+cnt = 0
+while True:
+    num += 1
+    if '666' in str(num):
+        cnt += 1
+
+        if cnt == N:
+            break
+
+print(num)
